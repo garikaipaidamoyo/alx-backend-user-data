99511081feda136db7df8051ca218b97da988683
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-11-02 08:12:50 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,17 +9,17 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    REDACTION = 'xxx'
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
     def format(self, record: logging.LogRecord) -> str:
         redacted_message = record.getMessage()
         fields = os.environ.get("FIELDS", "").split(",")
         for field in fields:
-            redacted_message = re.sub(f"{field}=[^;]+",
-                                      f"{field}={self.REDACTION}",
-                                      redacted_message)
+            redacted_message = re.sub(
+                f"{field}=.*?{self.SEPARATOR}",
+                f"{field}={self.REDACTION}{self.SEPARATOR}", redacted_message)
         record.message = redacted_message
         return super().format(record)
 
@@ -31,21 +31,9 @@ def filter_datum(fields, redaction, message, separator):
     return message
 
 
-filter_datum = __import__('filtered_logger').filter_datum
-
-fields = ["password", "date_of_birth"]
-messages = [
-    "name=egg;email=eggmin@eggsample.com;password=eggcellent;"
-    "date_of_birth=12/12/1986;"
-    "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
-]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
-
 if __name__ == "__main__":
-    message = "name=Bob;email=bob@dylan.com;"
-    "ssn=000-123-0000;password=bobby2019;"
+    message = ("name=Bob;email=bob@dylan.com;"
+               "ssn=000-123-0000;password=bobby2019;")
     log_record = logging.LogRecord("my_logger", logging.INFO, None, None,
                                    message, None, None)
     formatter = RedactingFormatter()
