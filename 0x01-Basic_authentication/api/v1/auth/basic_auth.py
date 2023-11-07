3c8132807c2ae82c860a8bbb8b35341ad5da82f4
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-11-07 19:22:12 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if not authorization_header or not isinstance(
+                authorization_header,
+                str) or not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.replace("Basic ", "")
