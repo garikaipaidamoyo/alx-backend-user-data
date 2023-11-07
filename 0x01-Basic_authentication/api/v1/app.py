8aea49bf6ae33710be9cfc66ef3456b48154df98
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-11-07 17:50:36 +0000

    Add some code on the app.py file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,26 +13,36 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-
 app = Flask(__name__)
 auth = None
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+if getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 if getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
+
 
 def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ]
 
     if request.path not in excluded_paths:
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
